63f7e879114e2c83888c7aacdb309a475086c218
lyubogankov <lyubogankov13@gmail.com>
2023-04-02 16:17:18 -0700

    added code styling using prismjs!

--- scripts/businesscard.js
+++ scripts/businesscard.js
@@ -1,31 +1,36 @@
-const output = document.getElementById("output")
-const code = document.getElementById("input")
+const output = document.getElementById("output");
+const input = document.getElementById("input");
 
-function addToOutput(s) {
-    output.value += `${s}\n`
-    output.scrollTop = output.scrollHeight
+// https://pyodide.org/en/stable/usage/quickstart.html#alternative-example
+
+output.value = "initializing console output...\n";
+
+// initialize pyodide
+function replaceOutput(contents) { 
+    output.value = contents;
+    output.scrollTop = output.scrollHeight;
 }
+function appendToOutput(contents) { 
+    output.value += `${contents}\n`;
+    output.scrollTop = output.scrollHeight;
+}
+
+async function main() {
+    let pyodide = await loadPyodide({stdout: appendToOutput, stderr: appendToOutput});
+    output.value = "console output ready!\n";
+    return pyodide;
+}
+let pyodideReadyPromise = main();
 
 async function evaluatePython() {
-    addToOutput(`>>>${code.value}`)
-
+    let pyodide = await pyodideReadyPromise;
     try {
-        // Since pyodide 0.18.0, you must call loadPackagesFromImports() 
-        // to import any python packages referenced via import statements in your code.
-        // This function will no longer do it for you.
-        await pyodide.loadPackagesFromImports(code.value, addToOutput, addToOutput)
-        let result = await pyodide.runPythonAsync(code.value)
-    }
-    catch (e) {
-        addToOutput(`${e}`)
+        // await pyodide.loadPackagesFromImports(input.value, appendToOutput, appendToOutput);
+        // clear the screen
+        output.value = "";
+        // don't need to output this, just want the "print" statement to run!
+        let returnval = pyodide.runPython(input.textContent);
+    } catch (err) {
+        output.value = err;
     }
 }
-
-(async () => { // enable await 
-    output.value = 'Initializing...\n'
-
-    // init Pyodide
-    window.pyodide = await loadPyodide({ stdout: addToOutput, stderr: addToOutput }) // redirect stdout and stderr to addToOutput
-    output.value += 'Ready!\n'
-
-})(); // execute immediately
